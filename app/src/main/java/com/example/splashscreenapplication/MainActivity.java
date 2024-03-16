37d5ffd3179e052f28a19204afc410e6cb3956e8
Vishnu Venkatesh Jawalkar <jawalkarvishnu321@gmail.com>
2024-03-16 19:33:22 +0530

    1.0
    1.0

---
+++ app/src/main/java/com/example/splashscreenapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.splashscreenapplication;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView txt;
+    Animation anime_btn, anime_txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn = findViewById(R.id.button);
+        txt = findViewById(R.id.textView);
+
+
+        //animation
+        anime_btn = AnimationUtils.loadAnimation(this,R.anim.anime_btn);
+
+        btn.setAnimation(anime_btn);
+
+        //Anime for TextView
+        anime_txt = AnimationUtils.loadAnimation(this,R.anim.anime_txt);
+        txt.setAnimation(anime_txt);
+
+
+
+    }
+}
